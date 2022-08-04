d72d727a802097e8c3eea8739a4c148a09fd9e95
Caio Batista <caiobatis7@gmail.com>
2022-08-04 09:34:51 -0300

    feat: add initial praise data

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,15 +11,22 @@ export enum PraiseCategoryEnum {
 }
 
 export enum PraiseCDEnum {
+  EMPTY = "",
   CD_JOVEM_1992 = "CD Jovem 1992 - Nosso Sol é Jesus",
   CD_JOVEM_2005 = "CD Jovem 2005 - Fiel a Toda Prova",
+}
+
+export enum PraiseTypeEnum {
+  CIFRA = "Cifra",
+  PARTITURA = "Partitura",
 }
 
 export interface Praise {
   id: number
   title: string
   category: PraiseCategoryEnum
-  cd: PraiseCDEnum;
-  externalURL: string
-  imagePath: string
+  cd?: PraiseCDEnum;
+  externalURL?: string
+  imagePath?: string
+  type?: PraiseTypeEnum
 }
---
+++ utils/praises-data.ts
@@ -0,0 +1,13 @@
+import { Praise, PraiseCategoryEnum, PraiseCDEnum, PraiseTypeEnum } from '../interfaces'
+
+export const praiseData: Praise[] = [
+  {
+    id: 101,
+    title: "020 - Grande é o Senhor",
+    category: PraiseCategoryEnum.HIASD,
+    cd: PraiseCDEnum.EMPTY,
+    externalURL: "string",
+    imagePath: "string",
+    type: PraiseTypeEnum.CIFRA
+  }
+]
