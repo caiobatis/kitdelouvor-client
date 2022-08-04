b0e94a069926e2f26050cda1e96e74eebf1b54cf
Caio Batista <caiobatis7@gmail.com>
2022-08-04 09:29:04 -0300

    feat: add praises interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,10 +1,25 @@
-// You can include shared interfaces/types in a separate file
-// and then use them in any component by importing them. For
-// example, to import the interface below do:
-//
-// import { User } from 'path/to/interfaces';
-
 export type User = {
   id: number
   name: string
+}
+
+export enum PraiseCategoryEnum {
+  HIASD = "Hinário IASD",
+  NOVO_HIASD = "Novo Hinário IASD",
+  MINISTERIO_JOVEM = "Ministério Jovem",
+  ADORADORES = "Adoradores",
+}
+
+export enum PraiseCDEnum {
+  CD_JOVEM_1992 = "CD Jovem 1992 - Nosso Sol é Jesus",
+  CD_JOVEM_2005 = "CD Jovem 2005 - Fiel a Toda Prova",
+}
+
+export interface Praise {
+  id: number
+  title: string
+  category: PraiseCategoryEnum
+  cd: PraiseCDEnum;
+  externalURL: string
+  imagePath: string
 }
