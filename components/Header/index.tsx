ad69ae08c3fa256caeb4d3bedc8c18c0e965ae27
Caio Batista <caiobatis7@gmail.com>
2022-08-04 19:13:26 -0300

    feat: create initial home page

---
+++ components/Header/index.tsx
@@ -0,0 +1,38 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Stack,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+
+export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Container maxW='container.lg'>
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <Box>
+              <Text fontSize={18}>Kit de Louvor</Text>
+            </Box>
+
+            <Flex alignItems={'center'}>
+              <Stack direction={'row'} spacing={7}>
+                <Button onClick={toggleColorMode}>
+                  {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                </Button>
+              </Stack>
+            </Flex>
+          </Flex>
+        </Container>
+      </Box>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,57 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import {
+  Container,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  Button,
+} from '@chakra-ui/react';
+
+import Layout from '../components/Layout/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+  <Layout>
+    <Container maxW={'5xl'}>
+      <Stack
+        textAlign={'center'}
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+      >
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+          lineHeight={'110%'}
+        >
+          Encontre seu{' '}
+          <Text as={'span'} color={'orange.400'}>
+            Kit de Louvor
+          </Text>
+        </Heading>
+
+        <Text color={'gray.500'} maxW={'3xl'}>
+          Use a barra de busca abaixo para procurar pelo hino desejado. Separamos por título da música, CD e ano.
+        </Text>
+
+        <Stack spacing={6} direction={'row'} w={'full'}>
+          <Input 
+            rounded={'full'}
+            placeholder='Título, CD ou ano'
+            w={'full'}
+          />
+
+          <Button
+            rounded={'full'}
+            px={12}
+            colorScheme={'orange'}
+            bg={'orange.400'}
+            _hover={{ bg: 'orange.500' }}
+          >
+            Buscar
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
   </Layout>
 )
 
